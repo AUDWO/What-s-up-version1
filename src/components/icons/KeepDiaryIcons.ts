ea2fdd86fc10d29ba6a742bed9e19315bbf69595
audwo <kmjstj3@naver.com>
2024-02-05 00:06:54 +0900

    create:icons

---
+++ src/components/icons/KeepDiaryIcons.ts
@@ -0,0 +1,7 @@
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import styled from "styled-components";
+
+export const DownArrow = styled(IoIosArrowDown)``;
+
+export const UpArrow = styled(IoIosArrowUp)``;
